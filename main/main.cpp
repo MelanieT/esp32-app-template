9cfb854b783f44d09b31d16c5da6189a8f963a83
Melanie <melanie@t-data.com>
2025-03-07 12:06:13 +0000

    Remove wrongly placed submodule

--- main/main.cpp
+++ main/main.cpp
@@ -1,16 +1,211 @@
+#include <esp_spiffs.h>
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_mac.h>
 #include "main.h"
 #include "WiFi.h"
+#include "CppConsole.h"
+#include "ota.h"
+#include "SimpleWebServer.h"
+#include "CPPNVS.h"
+#include "EventHandler.h"
+#include "captdns.h"
 
+using namespace std;
+
+static const char *TAG = "filtermeter";
+
+Ota ota;
+Console *console = nullptr;
 WiFi wifi;
 AppMain appMain;
+esp_netif_t *ap_netif; // For the DNS
+
+void AppMain::processCommand(vector<string> args) // Do not move, do not pass by ref, we WANT a copy
+{
+    if (args.empty())
+        return;
+
+    if (args[0] == "update")
+    {
+        SimpleWebServer::stop_webserver(m_webserver);
+
+        spiffsUpdate((m_otaServer + "/spiffs.bin").c_str());
+        firmwareUpdate((m_otaServer + "/rom.img").c_str());
+    }
+
+    if (args[0] == "spiffsupdate")
+    {
+        spiffsUpdate((m_otaServer + "/spiffs.bin").c_str());
+        esp_restart();
+    }
+
+    if (args[0] == "reboot")
+    {
+        esp_restart();
+    }
+}
 
 extern "C" {
 void app_main()
 {
+    appMain.run();
 }
 }
 
 void AppMain::run()
 {
+    m_otaServer = "http://192.168.201.50/filtermeter/filtermeter";
 
+    NVS nvs("ls");
+
+    mountStorage("/data");
+
+    // TODO: Init API
+
+    if (nvs.get("ssid", &m_ssid))
+    {
+        m_ssid.clear();
+    }
+    if (nvs.get("password", &m_password))
+    {
+        m_password.clear();
+    }
+
+    printf("ssid %s\r\n", m_ssid.c_str());
+
+    m_hostname = generateHostname(TAG);
+
+    wifi.setStationHostname(generateHostname(TAG));
+    wifi.setWifiEventHandler(new EventHandler());
+
+    if (!m_ssid.empty() && !m_password.empty())
+        wifi.connectAP(m_ssid, m_password, false, WIFI_MODE_APSTA);
+    else
+        enterApMode();
+
+    vTaskDelete(nullptr);
+}
+
+void AppMain::enterApMode()
+{
+    if (m_apMode)
+        return;
+
+    printf("Entering AP mode\r\n");
+
+    m_apMode = true;
+
+    esp_wifi_stop();
+    wifi.startAP(generateHostname(TAG), "", WIFI_AUTH_OPEN);
+
+    ap_netif = wifi.getAccessPointIf();
+}
+
+void AppMain::apStarted()
+{
+    captdnsInit();
+    m_webserver = SimpleWebServer::start_webserver("/data");
+}
+
+void AppMain::apStopped()
+{
+    SimpleWebServer::stop_webserver(m_webserver);
+    captdnsStop();
+}
+
+
+esp_err_t AppMain::unmountStorage()
+{
+    esp_vfs_spiffs_unregister("spiffs");
+}
+
+esp_err_t AppMain::mountStorage(const char *basePath)
+{
+    ESP_LOGI(TAG, "Initializing SPIFFS");
+
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = basePath,
+        .partition_label = "spiffs",
+        .max_files = 5,   // This sets the maximum number of files that can be open at the same time
+        .format_if_mount_failed = false
+    };
+
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return ret;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info("spiffs", &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+        return ret;
+    }
+
+    ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    return ESP_OK;
+}
+
+string AppMain::generateHostname(const string& hostname_base)
+{
+    uint8_t chipid[6];
+    esp_read_mac(chipid, ESP_MAC_WIFI_STA);
+    static char hostname[32];
+    snprintf(hostname, 32, "%s_%02x%02x%02x", hostname_base.c_str(), chipid[3], chipid[4], chipid[5]);
+
+    return {hostname};
+}
+
+void AppMain::tryConnectWifi(std::string ssid, std::string password)
+{
+//    esp_wifi_disconnect();
+//    esp_wifi_stop();
+
+    if (m_testConnection)
+        return;
+
+    m_testConnection = true;
+
+    printf("Attempting to connect\r\n");
+    auto ret = wifi.connectAP(ssid, password, true, WIFI_MODE_APSTA);
+    printf("Connect done, result %d\r\n", ret);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    m_testConnection = false;
+
+    if (ret)
+    {
+        esp_wifi_disconnect();
+        esp_wifi_set_mode(WIFI_MODE_AP);
+        return;
+    }
+    m_stationConnected = true;
+
+    NVS nvs("ls");
+
+    nvs.set("ssid", ssid);
+    nvs.set("password", password);
+
+    nvs.commit();
+}
+
+void AppMain::spiffsUpdate(const char *from)
+{
+    ota_spiffs(from);
+}
+
+void AppMain::firmwareUpdate(const char *from)
+{
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    ota.update("http://192.168.201.50/lightswitch/lightswitch/rom.img");
+
+    esp_restart();
 }
--- main/main.h
+++ main/main.h
@@ -1,5 +1,7 @@
 #include "WiFi.h"
-#include "string"
+#include <string>
+#include <vector>
+#include "esp_http_server.h"
 
 extern WiFi wifi;
 
@@ -10,11 +12,35 @@ public:
 
     void run();
     inline std::string hostname() { return m_hostname; };
-    inline bool stationConnected() const { return m_stationConnected; };
+    [[nodiscard]] inline bool stationConnected() const { return m_stationConnected; };
 
 private:
+    void processCommand(std::vector<std::string> args);
+    esp_err_t mountStorage(const char *basePath);
+    esp_err_t unmountStorage();
+
     std::string m_hostname;
     bool m_stationConnected;
+    httpd_handle_t m_webserver;
+    std::string m_otaServer;
+    std::string m_ssid;
+    std::string m_password;
+    bool m_apMode = false;
+    bool m_testConnection = false;
+
+    std::string generateHostname(const std::string &hostname_base);
+
+    void enterApMode();
+
+    void apStarted();
+
+    void apStopped();
+
+    void tryConnectWifi(std::string ssid, std::string password);
+
+    void spiffsUpdate(const char *from);
+
+    void firmwareUpdate(const char *from);
 };
 
 extern AppMain appMain;
